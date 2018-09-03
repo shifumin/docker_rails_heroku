a305958e9ffb4972ced355c1948e5bbd958a57a8
shifumin <shifumin4230@gmail.com>
2018-09-03 23:01:00 +0900

    add deploy_dev jobs for CircleCI settings

---
+++ .circleci/heroku-container-release-dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+imageId=$(docker inspect registry.heroku.com/${HEROKU_APP_NAME_DEV}/web:latest --format={{.Id}})
+payload='{"updates":[{"type":"web","docker_image":"'"$imageId"'"}]}'
+curl -n -X PATCH https://api.heroku.com/apps/${HEROKU_APP_NAME_DEV}/formation \
+-d "$payload" \
+-H "Content-Type: application/json" \
+-H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+-H "Authorization: Bearer $HEROKU_AUTH_TOKEN"
